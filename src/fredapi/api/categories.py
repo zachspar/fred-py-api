fe844a52e5c1a4fa0339c3de8a331059f54c71a6
Zachary Spar <zachspar@gmail.com>
2022-07-02 14:36:48 -0400

    add api arg validator for categories

--- src/fredapi/api/categories.py
+++ src/fredapi/api/categories.py
@@ -3,6 +3,7 @@
 Fred API for Categories Namespace.
 """
 from .fred_client import FredClient
+from ..util.decorators import validate_api_args
 
 
 class FredAPICategories(FredClient):
@@ -18,26 +19,85 @@ class FredAPICategories(FredClient):
         fred/category/related_tags - Get the related tags for a category.
     """
 
+    @validate_api_args("api_key", "file_type", "category_id")
     def get_category(self, category_id: int = None, **kwargs) -> dict:
         """Get category by ID."""
-        return self._get(f"category", {"category_id": category_id, **kwargs, })
+        return self._get(f"category", {"category_id": category_id, **kwargs,})
 
+    @validate_api_args(
+        "api_key",
+        "file_type",
+        "category_id",
+        "realtime_start",
+        "realtime_end",
+        "limit",
+        "offset",
+        "order_by",
+        "sort_order",
+        "filter_variable",
+        "filter_value",
+        "tag_names",
+        "exclude_tag_names",
+    )
     def get_category_series(self, category_id: int = None, **kwargs) -> dict:
         """Get category series by ID."""
-        return self._get(f"category/series", {"category_id": category_id, **kwargs, })
+        return self._get(f"category/series", {"category_id": category_id, **kwargs,})
 
+    @validate_api_args("api_key", "file_type", "category_id", "realtime_start", "realtime_end")
     def get_category_children(self, category_id: int = None, **kwargs) -> dict:
         """Get category children by ID."""
-        return self._get(f"category/children", {"category_id": category_id, **kwargs, })
+        return self._get(f"category/children", {"category_id": category_id, **kwargs,})
 
+    @validate_api_args("api_key", "file_type", "category_id", "realtime_start", "realtime_end")
     def get_category_related(self, category_id: int = None, **kwargs) -> dict:
         """Get category related by ID."""
-        return self._get(f"category/related", {"category_id": category_id, **kwargs, })
+        return self._get(f"category/related", {"category_id": category_id, **kwargs,})
 
+    @validate_api_args(
+        "api_key",
+        "file_type",
+        "category_id",
+        "realtime_start",
+        "realtime_end",
+        "limit",
+        "offset",
+        "order_by",
+        "sort_order",
+        "filter_variable",
+        "filter_value",
+        "tag_names",
+        "tag_group_id",
+        "search_text",
+        "limit",
+        "offset",
+        "order_by",
+        "sort_order",
+    )
     def get_category_tags(self, category_id: int = None, **kwargs) -> dict:
         """Get category tags by ID."""
         raise NotImplementedError("get_category_tags endpoint is not yet implemented.")
 
+    @validate_api_args(
+        "api_key",
+        "file_type",
+        "category_id",
+        "realtime_start",
+        "realtime_end",
+        "limit",
+        "offset",
+        "order_by",
+        "sort_order",
+        "filter_variable",
+        "filter_value",
+        "tag_names",
+        "exclude_tag_names",
+        "tag_group_id",
+        "search_text",
+        "limit",
+        "offset",
+        "order_by",
+        "sort_order",
+    )
     def get_category_related_tags(self, category_id: int = None, **kwargs) -> dict:
         """Get category related tags by ID."""
         raise NotImplementedError("get_category_related_tags endpoint is not yet implemented.")
