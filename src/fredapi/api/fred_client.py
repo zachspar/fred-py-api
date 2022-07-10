7e6846f6671c896846689373cf0d4a57397a5cca
Zachary Spar <zachspar@gmail.com>
2022-07-10 19:53:22 -0400

    check alnum

--- src/fredapi/api/fred_client.py
+++ src/fredapi/api/fred_client.py
@@ -28,7 +28,7 @@ class FredClient(object):
         else:
             raise TypeError(f"Invalid init type")
 
-        if len(self._api_key) != 32:
+        if not self._api_key.isalnum() or len(self._api_key) != 32:
             raise ValueError("Invalid API Key - must be 32 characters in length")
 
         self.base_url = "https://api.stlouisfed.org/fred"
