a8fce9830dae2823492892e9ddd5c27978ae01c1
Zachary Spar <41600414+zachspar@users.noreply.github.com>
2022-07-14 19:31:12 -0400

    Add support for XML `file_type` (#18)
    * add XML support, and associated test
    
    * bump lib version to v0.1.1

--- src/fred/api/series.py
+++ src/fred/api/series.py
@@ -2,8 +2,8 @@
 """
 Fred API Series Namespace Requests.
 """
-from .fred_client import FredClient
-from ..util.decorators import validate_api_args
+from ._fred_client import FredClient
+from .._util.decorators import validate_api_args
 
 
 class FredAPISeries(FredClient):
