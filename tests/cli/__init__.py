3480a19b8ab7655cae53281e1c4245be9fda179f
Zachary Spar <zachspar@gmail.com>
2022-09-22 22:36:54 -0400

    allow for more testing constraints in base framework

--- tests/cli/__init__.py
+++ tests/cli/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os
+from json import loads
 from typing import List, Dict
 from unittest import TestCase, mock
 
@@ -16,9 +17,17 @@ class BaseCLITest(TestCase):
         self.patcher = mock.patch.dict(os.environ, {"FRED_API_KEY": os.environ.get("TEST_FRED_API_KEY__CLI")})
         self.patcher.start()
         self.client = FredAPI(api_key=os.environ.get("FRED_API_KEY"))
+
+        # set basic context object for all cli tests
         self.obj = {
             "api_key": self.client.get_api_key(),
             "client": self.client,
+        }
+
+        # set basic params for all requests
+        self.base_params = {
+            "api_key": self.client.get_api_key(),
+            "file_type": "json",
         }
 
     def tearDown(self) -> None:
@@ -35,13 +44,21 @@ class BaseCLITest(TestCase):
 
             with self.subTest(case["msg"], exit_code=exit_code, command=command):
 
-                if "raises" in case_keys:
-                    with self.assertRaises(case["raises"]):
-                        result = self.runner.invoke(cli, command, obj=self.obj)
-                else:
-                    result = self.runner.invoke(cli, command, obj=self.obj)
+                # get cli result
+                result = self.runner.invoke(cli, command, obj=self.obj)
 
-                self.assertEqual(case["exit_code"], result.exit_code)
-
+                # assert output
                 if "output" in case_keys:
-                    self.assertEqual(case["output"], result.output)
+                    if isinstance(case["output"], dict):
+                        comparison_keys = set(case["output"].keys())
+                        if "dict" in comparison_keys:
+                            self.assertDictEqual(case["output"]["dict"], loads(result.output))
+                        elif "contains" in comparison_keys:
+                            self.assertIn(case["output"]["contains"], result.output)
+                        else:
+                            self.assertEqual(case["output"], result.output)
+                    else:
+                        self.assertEqual(case["output"], result.output)
+
+                # assert exit code
+                self.assertEqual(case["exit_code"], result.exit_code)
