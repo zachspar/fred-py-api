47559b1b2a9bf5aea16afb87f8d2c8fcc5ecdf20
Zachary Spar <zachspar@gmail.com>
2022-09-24 18:56:10 -0400

    finish up all basic tests for series cli

--- tests/cli/test_series_cli.py
+++ tests/cli/test_series_cli.py
@@ -117,3 +117,170 @@ class TestCLISeries(BaseCLITest):
             },
         ]
         self.run_test_cases(series, tests)
+
+    def test_get_series_search(self):
+        """CLI test for get-series-search."""
+        tests = [
+            {
+                "msg": "Basic get-series-search test Ok",
+                "exit_code": 0,
+                "command": ["get-series-search", "--search-text", "monetary+service+index"],
+                "output": {
+                    "dict": get(
+                        "https://api.stlouisfed.org/fred/series/search",
+                        params={"search_text": "monetary+service+index", **self.base_params},
+                    ).json(),
+                },
+            },
+            {
+                "msg": "Basic get-series-search test Fail",
+                "exit_code": 2,
+                "command": ["get-series-search", "-t", "monetary+service+index", "asdf=asdf"],
+                "output": {
+                    "contains": "Error: asdf is not a valid argument for get_series_search.",
+                },
+            },
+        ]
+        self.run_test_cases(series, tests)
+
+    def test_get_series_search_tags(self):
+        """CLI test for get-series-search-tags."""
+        tests = [
+            {
+                "msg": "Basic get-series-search-tags test Ok",
+                "exit_code": 0,
+                "command": ["get-series-search-tags", "-t", "monetary+service+index"],
+                "output": {
+                    "dict": get(
+                        "https://api.stlouisfed.org/fred/series/search/tags",
+                        params={"series_search_text": "monetary+service+index", **self.base_params},
+                    ).json(),
+                },
+            },
+            {
+                "msg": "Basic get-series-search-tags test Fail",
+                "exit_code": 2,
+                "command": ["get-series-search-tags", "-t", "monetary+service+index", "asdf=asdf"],
+                "output": {
+                    "contains": "Error: asdf is not a valid argument for get_series_search_tags.",
+                },
+            },
+        ]
+        self.run_test_cases(series, tests)
+
+    def test_get_series_search_related_tags(self):
+        """CLI test for get-series-search-related-tags using series_search_text=mortgage+rate&tag_names=30-year;frb"""
+        tests = [
+            {
+                "msg": "Basic get-series-search-related-tags test Ok",
+                "exit_code": 0,
+                "command": [
+                    "get-series-search-related-tags",
+                    "-t",
+                    "mortgage+rate",
+                    "-n",
+                    "30-year;frb",
+                ],
+                "output": {
+                    "dict": get(
+                        "https://api.stlouisfed.org/fred/series/search/related_tags",
+                        params={
+                            "series_search_text": "mortgage+rate",
+                            "tag_names": "30-year;frb",
+                            **self.base_params,
+                        },
+                    ).json(),
+                },
+            },
+            {
+                "msg": "Basic get-series-search-related-tags test Fail",
+                "exit_code": 2,
+                "command": [
+                    "get-series-search-related-tags",
+                    "-t",
+                    "mortgage+rate",
+                    "-n",
+                    "30-year;frb",
+                    "asdf=asdf",
+                ],
+                "output": {
+                    "contains": "Error: asdf is not a valid argument for get_series_search_related_tags.",
+                },
+            },
+        ]
+        self.run_test_cases(series, tests)
+
+    def test_get_series_tags(self):
+        """CLI test for get-series-tags."""
+        tests = [
+            {
+                "msg": "Basic get-series-tags test Ok",
+                "exit_code": 0,
+                "command": ["get-series-tags", "-i", "dff"],
+                "output": {
+                    "dict": get(
+                        "https://api.stlouisfed.org/fred/series/tags",
+                        params={"series_id": "dff", **self.base_params},
+                    ).json(),
+                },
+            },
+            {
+                "msg": "Basic get-series-tags test Fail",
+                "exit_code": 2,
+                "command": ["get-series-tags", "-i", "dff", "asdf=asdf"],
+                "output": {
+                    "contains": "Error: asdf is not a valid argument for get_series_tags.",
+                },
+            },
+        ]
+        self.run_test_cases(series, tests)
+
+    def test_get_series_updates(self):
+        """CLI test for get-series-updates."""
+        tests = [
+            {
+                "msg": "Basic get-series-updates test Ok",
+                "exit_code": 0,
+                "command": ["get-series-updates"],
+                "output": {
+                    "dict": get(
+                        "https://api.stlouisfed.org/fred/series/updates",
+                        params=self.base_params,
+                    ).json(),
+                },
+            },
+            {
+                "msg": "Basic get-series-updates test Fail",
+                "exit_code": 2,
+                "command": ["get-series-updates", "asdf=asdf"],
+                "output": {
+                    "contains": "Error: asdf is not a valid argument for get_series_updates.",
+                },
+            },
+        ]
+        self.run_test_cases(series, tests)
+
+    def test_get_series_vintagedates(self):
+        """CLI test for get-series-vintagedates."""
+        tests = [
+            {
+                "msg": "Basic get-series-vintagedates test Ok",
+                "exit_code": 0,
+                "command": ["get-series-vintagedates", "-i", "GNPCA"],
+                "output": {
+                    "dict": get(
+                        "https://api.stlouisfed.org/fred/series/vintagedates",
+                        params={"series_id": "GNPCA", **self.base_params},
+                    ).json(),
+                },
+            },
+            {
+                "msg": "Basic get-series-vintagedates test Fail",
+                "exit_code": 2,
+                "command": ["get-series-vintagedates", "-i", "GNPCA", "asdf=asdf"],
+                "output": {
+                    "contains": "Error: asdf is not a valid argument for get_series_vintagedates.",
+                },
+            },
+        ]
+        self.run_test_cases(series, tests)
