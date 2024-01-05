a5122cac0d7a84fa4e9f4e3d97af4a1f39626bb8
Zachary Spar <zachspar@gmail.com>
2024-01-04 22:30:43 -0500

    add more utility tests

--- tests/cli/test_cli_entry_point.py
+++ tests/cli/test_cli_entry_point.py
@@ -5,7 +5,7 @@ from tests.cli import BaseCLITest
 class TestCLIEntryPoint(BaseCLITest):
     """Test CLI entry point."""
 
-    def test_cli_entry_point_import_and_run(self):
+    def test_fred_cli_entry_point_import_and_run(self):
         """Test the CLI entry point imports properly."""
         from fred.cli.__main__ import run_fred_cli
 
@@ -14,3 +14,53 @@ class TestCLIEntryPoint(BaseCLITest):
         # make sure CLI exits with sys.exit()
         with self.assertRaises(SystemExit):
             run_fred_cli()
+
+    def test_categories_cli_entry_point_import_and_run(self):
+        """Test the CLI entry point imports properly."""
+        from fred.cli.categories import run_categories_cli
+
+        self.assertIsNotNone(run_categories_cli)
+
+        # make sure CLI exits with sys.exit()
+        with self.assertRaises(SystemExit):
+            run_categories_cli()
+
+    def test_releases_cli_entry_point_import_and_run(self):
+        """Test the CLI entry point imports properly."""
+        from fred.cli.releases import run_releases_cli
+
+        self.assertIsNotNone(run_releases_cli)
+
+        # make sure CLI exits with sys.exit()
+        with self.assertRaises(SystemExit):
+            run_releases_cli()
+
+    def test_series_cli_entry_point_import_and_run(self):
+        """Test the CLI entry point imports properly."""
+        from fred.cli.series import run_series_cli
+
+        self.assertIsNotNone(run_series_cli)
+
+        # make sure CLI exits with sys.exit()
+        with self.assertRaises(SystemExit):
+            run_series_cli()
+
+    def test_sources_cli_entry_point_import_and_run(self):
+        """Test the CLI entry point imports properly."""
+        from fred.cli.sources import run_sources_cli
+
+        self.assertIsNotNone(run_sources_cli)
+
+        # make sure CLI exits with sys.exit()
+        with self.assertRaises(SystemExit):
+            run_sources_cli()
+
+    def test_tags_cli_entry_point_import_and_run(self):
+        """Test the CLI entry point imports properly."""
+        from fred.cli.tags import run_tags_cli
+
+        self.assertIsNotNone(run_tags_cli)
+
+        # make sure CLI exits with sys.exit()
+        with self.assertRaises(SystemExit):
+            run_tags_cli()
--- tests/cli/test_cli_utils.py
+++ tests/cli/test_cli_utils.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 import unittest
+from typing import Any
 from xml.etree import ElementTree as ET
 
-from fred._util import generate_api_kwargs, serialize
+import click
+
+from fred._util import generate_api_kwargs, serialize, run_cli_callable, init_cli_context
 
 
 class TestCLIUtils(unittest.TestCase):
@@ -34,3 +37,35 @@ class TestCLIUtils(unittest.TestCase):
         response_obj = "invalid"
         with self.assertRaises(TypeError):
             serialize(response_obj)
+
+    def test_run_cli_callable(self):
+        """Test the run_cli_callable function."""
+
+        def fake_cli_callable(auto_envvar_prefix: Any = None):
+            raise AssertionError("Fake Fred API callable.")
+
+        self.assertIsNone(run_cli_callable(fake_cli_callable))
+
+    def test_init_cli_context(self):
+        """Test the init_cli_context function."""
+
+        @click.command()
+        @click.pass_context
+        def fake_cli_command(ctx: click.Context):
+            pass
+
+        class FakeAPIClient:
+            def __init__(self, api_key: str):
+                pass
+
+        ctx = click.Context(command=fake_cli_command)
+        api_key = "fake_api_key"
+        api_client_class = FakeAPIClient
+
+        init_cli_context(ctx, api_key, api_client_class)
+
+        # check that api_key is identical
+        self.assertEqual(ctx.obj["api_key"], api_key)
+
+        # check that context client is of type FakeAPIClient
+        self.assertTrue(isinstance(ctx.obj["client"], api_client_class))
