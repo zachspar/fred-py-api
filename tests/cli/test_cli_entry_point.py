89f8caec41a337f2de40d755f9a0c5ccf09e4d5d
Zachary Spar <zachspar@gmail.com>
2024-01-04 23:50:36 -0500

    add more cli entrypoint tests for each subcommand

--- tests/cli/test_cli_entry_point.py
+++ tests/cli/test_cli_entry_point.py
@@ -15,15 +15,59 @@ class TestCLIEntryPoint(BaseCLITest):
         with self.assertRaises(SystemExit):
             run_fred_cli()
 
-    def test_fred_cli_init_entrypoint_import_and_run(self):
+    def test_fred_cli_entrypoint(self):
         """Test the CLI entry point from __init__."""
         from fred.cli import fred_cli
 
         self.assertIsNotNone(fred_cli)
 
-        # make sure CLI exits with sys.exit()
-        with self.assertRaises(SystemExit):
-            fred_cli()
+        result = self.runner.invoke(fred_cli, ["--help"])
+        self.assertEqual(result.exit_code, 0)
+
+    def test_categories_cli_entrypoint(self):
+        """Test the CLI entry point from categories."""
+        from fred.cli.categories import categories
+
+        self.assertIsNotNone(categories)
+
+        result = self.runner.invoke(categories, ["--help"])
+        self.assertEqual(result.exit_code, 0)
+
+    def test_releases_cli_entrypoint(self):
+        """Test the CLI entry point from releases."""
+        from fred.cli.releases import releases
+
+        self.assertIsNotNone(releases)
+
+        result = self.runner.invoke(releases, ["--help"])
+        self.assertEqual(result.exit_code, 0)
+
+    def test_series_cli_entrypoint(self):
+        """Test the CLI entry point from series."""
+        from fred.cli.series import series
+
+        self.assertIsNotNone(series)
+
+        result = self.runner.invoke(series, ["--help"])
+        self.assertEqual(result.exit_code, 0)
+
+    def test_sources_cli_entrypoint(self):
+        """Test the CLI entry point from sources."""
+        from fred.cli.sources import sources
+
+        self.assertIsNotNone(sources)
+
+        result = self.runner.invoke(sources, ["--help"])
+        self.assertEqual(result.exit_code, 0)
+
+    def test_tags_cli_entrypoint(self):
+        """Test the CLI entry point from tags."""
+        from fred.cli.tags import tags
+
+        self.assertIsNotNone(tags)
+
+        result = self.runner.invoke(tags, ["--help"])
+        self.assertEqual(result.exit_code, 0)
 
     def test_categories_cli_entry_point_import_and_run(self):
         """Test the CLI entry point imports properly."""
