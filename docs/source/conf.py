5a55239aa6f54e3b233e4130e625680d7008476e
Zachary Spar <zachspar@gmail.com>
2024-06-29 01:19:20 -0400

    update readme, include auto-completion docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath(os.path.join("..", "..", "src")))
 project = "fred-py-api"
 copyright = "2024, Zachary Spar"
 author = "Zachary Spar"
-release = "1.1.3"
+release = "1.2.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
